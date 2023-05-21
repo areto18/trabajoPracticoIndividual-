0c9721be6a1b7760a740d69b47f748fcb1b3da0f
areto18 <areto18@gmail.com>
2023-05-20 21:08:24 -0300

    styles css

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/public')));
+/**Ejemplo para ver
+ * const publicPath = path.resolve(__dirname. './public');
+ * app.use( express.static(publicPath) );
+ */
+
+app.use(express.static(path.join(__dirname, '/public'))); //setear la carpeta de archivos publicos
 
 
 app.listen(3002, function () {
     console.log("Servidor corriendo");
 });
+
+
+ app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
+ })
+ 
