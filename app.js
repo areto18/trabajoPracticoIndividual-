bec70f06855c8a291c5b35d8f684066979a1c755
areto18 <areto18@gmail.com>
2023-06-04 02:47:43 -0300

    register y login

--- app.js
+++ app.js
@@ -10,13 +10,18 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '/public'))); //setear la carpeta de archivos publicos
 
-
-app.listen(3002, function () {
-    console.log("Servidor corriendo");
-});
-
-
  app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
- })
+ });
  
+ app.get('/formulario', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+ });
+
+ app.get('/ingresar', (req, res) => {
+   res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
+
+ app.listen(3002, function () {
+   console.log("Servidor corriendo");
+});
